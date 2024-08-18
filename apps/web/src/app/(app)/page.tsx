960b73364be18d0ceb49a213cf19cb35015ffd71
Rubens <rubensgouveia@ioasys.com.br>
2024-08-18 12:05:39 -0300

    linting fix

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/components/header'
 
 export default async function Home() {
   const { user } = await auth()
+  console.log('user', user)
   return (
     <>
       <Header />
